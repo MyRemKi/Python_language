358a2020aa90e47b81cd984f59ea19a1370345dd
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2025-02-24 14:03:27 +0100

    Update Pandas_And_Numpy.py

--- Pandas/Pandas_And_Numpy.py
+++ Pandas/Pandas_And_Numpy.py
@@ -22,9 +22,13 @@ class Map:
     def Print(self):
         print(self.pd)
     
-    def Return_Data_columns(self):
-        return self.pd.columns
+    def Return_Data_columns(self,columns):
+        return self.pd[columns]
     
+    def Return_Data_Row(self,row):
+        print(self.pd.iloc[row])
+        return self.pd.iloc[row]
+        
     def Return_Data_Index_columns(self):
         return self.pd.columns
 
@@ -33,11 +37,18 @@ map_1 = Map(100,100)
 map_1.Initialize(-100,100)
 map_1.Print()
 print('------------------------------------------')
-LoopCount=10
-while LoopCount!=0:
+Loop=True
+while Loop==True:
     request = str(input("$:"))
     if(request=="exit"):
-        LoopCount=0
-    if(request=="return -col"):
-        print(map_1.Return_Data_columns())
+        Loop=False
+    if(request=="return -data=index"):
+        print(map_1.Return_Data_Index_columns())
+    
+    if("return -data -col=" in request) :
+        print(map_1.Return_Data_columns(request[18:]))
+    
+    if("return -data -row=" in request) :
+        print(request[18:])
+        print(Return_Data_Row(request[18:]))
     
